392a3681ff33399c270e7296363ce8458b95bb23
prma85 <paulo85br@gmail.com>
2020-02-04 01:02:27 -0600

    new index

--- website/src/pages/index.js
+++ website/src/pages/index.js
@@ -13,54 +13,6 @@ import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
 import useBaseUrl from '@docusaurus/useBaseUrl';
 import styles from './styles.module.css';
 
-const features = [
-  {
-    title: <>Easy to Use</>,
-    imageUrl: 'img/undraw_docusaurus_mountain.svg',
-    description: (
-      <>
-        Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
-      </>
-    ),
-  },
-  {
-    title: <>Focus on What Matters</>,
-    imageUrl: 'img/undraw_docusaurus_tree.svg',
-    description: (
-      <>
-        Docusaurus lets you focus on your docs, and we&apos;ll do the chores. Go
-        ahead and move your docs into the <code>docs</code> directory.
-      </>
-    ),
-  },
-  {
-    title: <>Powered by React</>,
-    imageUrl: 'img/undraw_docusaurus_react.svg',
-    description: (
-      <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
-      </>
-    ),
-  },
-];
-
-function Feature({imageUrl, title, description}) {
-  const imgUrl = useBaseUrl(imageUrl);
-  return (
-    <div className={classnames('col col--4', styles.feature)}>
-      {imgUrl && (
-        <div className="text--center">
-          <img className={styles.featureImage} src={imgUrl} alt={title} />
-        </div>
-      )}
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </div>
-  );
-}
-
 function Home() {
   const context = useDocusaurusContext();
   const {siteConfig = {}} = context;
@@ -78,24 +30,32 @@ function Home() {
                 'button button--outline button--secondary button--lg',
                 styles.getStarted,
               )}
-              to={useBaseUrl('docs/doc1')}>
+              to={useBaseUrl('docs/01-intro')}>
               Get Started
             </Link>
           </div>
         </div>
       </header>
       <main>
-        {features && features.length && (
           <section className={styles.features}>
             <div className="container">
               <div className="row">
-                {features.map((props, idx) => (
-                  <Feature key={idx} {...props} />
-                ))}
+                <div className="col">
+                <h3>Resources</h3>
+                  <ul>
+                  <li><strong>Slack</strong>: Joined the slack through the workspace link: <a href="https://noderegjan23.slack.com">Node.js Regina 2020</a></li>
+                  <li><strong>Articles</strong>: Check the <a href="https://medium.com/topic/javascript">Medium.com</a> for hot topics on Javascript</li>
+                  <li><strong>Extra Content</strong>: Several examples ara avaliable at <a href="http://www.w3schools.com/">W3Schools</a></li>
+                  <li><strong>JavaScript new Features</strong>: An excellent reference can be found on <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Introduction">MDN Web Docs</a></li>
+                  </ul>
+
+                <h3>Content</h3>
+
+<p>This is the official course program! The course content will be distributed on a weekly basis, feel free to read it all and even pull request (more on that on the git lesson) suggestions.
+ </p>               </div>
               </div>
             </div>
           </section>
-        )}
       </main>
     </Layout>
   );
