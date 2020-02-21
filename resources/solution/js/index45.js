9a862ae38579e72426c4da3b996f52159f371b02
prma85 <paulo85br@gmail.com>
2020-02-20 21:51:51 -0600

    Update index45.js

--- resources/solution/js/index45.js
+++ resources/solution/js/index45.js
@@ -6,7 +6,9 @@ const slice1 = playerName.slice(0, -5);
 const slice2 = teams.slice(66, 72);
 const slice3 = message.slice(10, 18);
 const slice4 = message.slice(-11, -7);
-const slice2Changed = (slice2.charAt(0).toUpperCase()).concat(slice2.slice(1));
+
+const slice2FirstLetter = slice2.charAt(0).toUpperCase();
+const slice2Changed = slice2FirstLetter.concat(slice2.slice(1));
 
 const result = `${slice1.toUpperCase()} ${slice3} ${slice2Changed} player ${slice4} ${slice2Changed}`
 
