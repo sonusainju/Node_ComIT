1e8fc9b237703e537c5a4d2770c7922d6e7917cf
Paulo Andrade <pauloa@iqmetrix.com>
2020-03-26 23:27:38 -0600

    finalize express + node

--- resources/solution/node-site-example/index.js
+++ resources/solution/node-site-example/index.js
@@ -1,26 +1,45 @@
 const express = require("express");
 const app = express();
 const port = 3000;
+const superheroes = require("./superheroes");
+const bodyParser = require("body-parser");
+const urlencodedParser = bodyParser.urlencoded({ extended: false });
+
+app.set("view engine", "pug");
+app.use(express.static("public"));
+
+app.get("/", (req, res) => {
+  res.render("index", { superheroes: superheroes });
+});
+
+app.get("/superheroes/:id", (req, res) => {
+  const selectedId = req.params.id;
+  let selectedSuperhero = superheroes.filter(s => s.id === +selectedId);
+  selectedSuperhero = selectedSuperhero[0];
+
+  if (!selectedSuperhero) { // if the id doesn't exist, throw a 404 error
+    res.status(404).render("404", {url: req.url});
+  } else {
+    res.render("superhero", { superhero: selectedSuperhero });
+  }
+});
+
+app.post("/superheroes/", urlencodedParser, (req, res) => {
+  const newId = superheroes[superheroes.length - 1].id + 1;
+  const newSuperHero = {
+    id: newId,
+    name: req.body.superhero.toUpperCase(),
+    image: "lukecage.jpg"
+  };
+  superheroes.push(newSuperHero);
+
+  res.redirect("/");
+});
+
+app.use(function(req, res, next) {
+  res.status(404).render("404", {url: req.url});
+});
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
 });
-
-app.set('view engine', 'pug');
-
-app.get("/", (req, res) => {
-  const superheroes = [
-    { name: "SPIDER-MAN", image: "spiderman.jpg" },
-    { name: "CAPTAIN MARVEL", image: "captainmarvel.jpg" },
-    { name: "HULK", image: "hulk.jpg" },
-    { name: "THOR", image: "thor.jpg" },
-    { name: "IRON MAN", image: "ironman.jpg" },
-    { name: "DAREDEVIL", image: "daredevil.jpg" },
-    { name: "BLACK WIDOW", image: "blackwidow.jpg" },
-    { name: "CAPTAIN AMERICA", image: "captanamerica.jpg" },
-    { name: "WOLVERINE", image: "wolverine.jpg" }
-  ];
-
-  res.render("index", { superheroes: superheroes });
-});
-app.use(express.static('public'));
